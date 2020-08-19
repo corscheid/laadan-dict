56c4a9af8dd6c7d4b8b66f2c907eb52e199dd369
Corey Scheideman <corscheid@gmail.com>
2020-08-18 21:28:20 -0400

    Create Navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <Link href="/">
+        <a className="navbar-brand">Làadan Dictionary</a>
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link href="/">
+              <a className="nav-link">Home</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/about">
+              <a className="nav-link">About</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/users">
+              <a className="nav-link">Word List</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/api/users">API</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
