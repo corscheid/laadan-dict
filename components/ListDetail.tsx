571f24c3a6e9dbe1fe2f96a003c29eb5e8b00236
Corey Scheideman <corscheid@gmail.com>
2021-02-13 14:06:52 -0600

    Use page-title css class in h1

--- components/ListDetail.tsx
+++ components/ListDetail.tsx
@@ -8,7 +8,7 @@ type ListDetailProps = {
 
 const ListDetail = ({ item: user }: ListDetailProps) => (
   <div>
-    <h1>Detail for {user.name}</h1>
+    <h1 className="page-title">Detail for {user.name}</h1>
     <p>ID: {user.id}</p>
   </div>
 )
