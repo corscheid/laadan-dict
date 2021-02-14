fe46a7855852ca28031315cc20dd91f424bc1520
Corey Scheideman <corscheid@gmail.com>
2021-02-13 20:20:36 -0600

    Implement search feature, remove button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,32 @@
+import { useState } from 'react'
 import Layout from '../components/Layout'
+import List from '../components/List'
+import { dictionaryData } from '../utils/dictionary'
 
-const IndexPage = () => (
-  <Layout title="Láadan Dictionary">
-    <h1 className="page-title">Láadan ↔ English Dictionary</h1>
-    <div className="flex-container">
-      <form className="form-inline my-2 my-lg-0 search-form">
-        <input
-          className="form-control mr-sm-2 search-bar"
-          type="search"
-          placeholder="Search"
-          aria-label="Search"
-        />
-        <button
-          className="btn my-2 my-sm-0 search-btn"
-          type="submit"
-        >
-          Search
-        </button>
-      </form>
-      <div className="results"></div>
-    </div>
-  </Layout>
-)
+const IndexPage = () => {
+  const [text, setText] = useState("")
+
+  return (
+    <Layout title="Láadan Dictionary">
+      <h1 className="page-title">Láadan ↔ English Dictionary</h1>
+      <div className="flex-container">
+        <form className="form-inline my-2 my-lg-0 search-form">
+          <input
+            id="search"
+            className="form-control mr-sm-2 search-bar"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={text}
+            onChange={(e) => { setText(e.target.value) }}
+          />
+        </form>
+      </div>
+      <div className="results">
+        {text && <List items={dictionaryData.filter(w => (w.láadan.includes(text)) || w.english.includes(text))} />}
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
