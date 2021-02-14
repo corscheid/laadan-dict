098058089aa34d10c4feca0fdc2a98882df401eb
Corey Scheideman <corscheid@gmail.com>
2021-02-13 19:41:10 -0600

    Update ListDetail to bootstrap table

--- components/ListDetail.tsx
+++ components/ListDetail.tsx
@@ -1,16 +1,35 @@
 import * as React from 'react'
 
-import { User } from '../interfaces'
+import { Word } from '../interfaces'
 
 type ListDetailProps = {
-  item: User
+  item: Word
 }
 
-const ListDetail = ({ item: user }: ListDetailProps) => (
+const ListDetail = ({ item: word }: ListDetailProps) => (
   <div>
-    <h1 className="page-title">Detail for {user.name}</h1>
-    <p>ID: {user.id}</p>
+    <h1 className="page-title">{word.láadan}</h1>
+    <table className="table">
+      <tbody>
+        <tr>
+          <th scope="row">English:</th>
+          <td>{word.english}</td>
+        </tr>
+        <tr>
+          <th scope="row">Classification:</th>
+          <td>{word.classification}</td>
+        </tr>
+        <tr>
+          <th scope="row">Description:</th>
+          <td>{word.description}</td>
+        </tr>
+        {word.notes && <tr><th scope="row">Notes:</th><td>{word.notes}</td></tr>}
+        {word.wordBreakdown && <tr><th scope="row">Word breakdown:</th><td>{word.wordBreakdown}</td></tr>}
+        {word.unofficial && <tr><th scope="row">Unofficial:</th><td>{word.unofficial}</td></tr>}
+      </tbody>
+    </table>
   </div>
+
 )
 
 export default ListDetail
