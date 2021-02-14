43be2765c1a97abb5332f8ec1a6509ba574a66cd
Corey Scheideman <corscheid@gmail.com>
2021-02-13 19:42:43 -0600

    Add word by id page

---
+++ pages/words/[id].tsx
@@ -0,0 +1,58 @@
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { Word } from '../../interfaces'
+import Layout from '../../components/Layout'
+import ListDetail from '../../components/ListDetail'
+import { dictionaryData } from '../../utils/dictionary'
+
+type Props = {
+  item?: Word
+  errors?: string
+}
+
+const StaticPropsDetail = ({ item, errors }: Props) => {
+  if (errors) {
+    return (
+      <Layout title="Error">
+        <p>
+          <span style={{ color: 'red' }}>Error:</span> {errors}
+        </p>
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout
+      title={`${item ? item.láadan : 'Word Detail'}`}
+    >
+      {item && <ListDetail item={item} />}
+    </Layout>
+  )
+}
+
+export default StaticPropsDetail
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // Get the paths we want to pre-render based on users
+  const paths = dictionaryData.map((word) => ({
+    params: { id: word.id },
+  }))
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false }
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries.
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  try {
+    const id = params?.id
+    const item = dictionaryData.find((data) => data.id === id)
+    // By returning { props: item }, the StaticPropsDetail component
+    // will receive `item` as a prop at build time
+    return { props: { item } }
+  } catch (err) {
+    return { props: { errors: err.message } }
+  }
+}
