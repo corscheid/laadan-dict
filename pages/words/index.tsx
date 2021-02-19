a0252c5d8ec10aacf1b17bf893ff6fd9195beea8
Corey Scheideman <corscheid@gmail.com>
2021-02-19 10:29:24 -0600

    Remove hardcoded dictionary array

--- pages/words/index.tsx
+++ pages/words/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from 'next'
 import { Word } from '../../interfaces'
-import { dictionaryData } from '../../utils/dictionary'
+import { API_URL } from '../../utils/environment'
 import Layout from '../../components/Layout'
 import List from '../../components/List'
 
@@ -16,10 +16,8 @@ const WithStaticProps = ({ items }: Props) => (
 )
 
 export const getStaticProps: GetStaticProps = async () => {
-  // Example for including static props in a Next.js function component page.
-  // Don't forget to include the respective types for any props passed into
-  // the component.
-  const items: Word[] = dictionaryData
+  const response = await fetch(`${API_URL}/all`)
+  const items: Word[] = await response.json().catch(e => console.log(e))
   return { props: { items } }
 }
 
