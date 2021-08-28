67fad4a595711c440dd28bdf9acb4609869a6fcb
Corey Scheideman <corscheid@gmail.com>
2021-08-28 14:53:00 -0500

    Add meta tags for mobile home screens

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,6 +15,9 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="theme-color" content="#e7d7c1" />
     </Head>
     <header>
       <Navbar />
