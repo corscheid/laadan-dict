effa8626bfb609c5c7450929bf0a8e98ad9dcbf7
Corey Scheideman <corscheid@gmail.com>
2021-08-28 22:39:13 -0500

    Update credits

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,15 +6,15 @@ const AboutPage = () => (
     <h2 className="page-subtitle">Wil sha!</h2>
 
     <p>
-      This Láadan dictionary web app was created
-      by <a rel="noopener noreferrer" target="_blank" href="https://corscheid.dev">corscheid</a> (Corey Scheideman)
-      with the hope that anyone learning Láadan might find it useful.<br />
+      This Láadan dictionary web app was created with the hope that anyone learning Láadan might find it useful.<br />
       The source code is available
       on <a rel="noopener noreferrer" target="_blank" href="https://github.com/corscheid/laadan-dict">GitHub</a>
     </p>
 
     <h2 className="page-subtitle">Credits</h2>
     <ul>
+      <li>Developed by <a rel="noopener noreferrer" target="_blank" href="https://corscheid.dev">Corey Scheideman</a></li>
+      <li>UI designed by Morgan Hughes</li>
       <li>Láadan language created by Suzette Haden Elgin</li>
       <li>Initial dictionary data JSON data file compiled by Rachel Singh
         at <a rel="noopener noreferrer" target="_blank" href="http://data.ayadanconlangs.com/">data.ayadanconlangs.com</a>
