9c3bc8771ed3419cdca4ce3faa37a5a8d2d8c10a
Corey Scheideman <corscheid@gmail.com>
2021-09-14 21:56:04 -0500

    Fix GA SameSite/Domain on route change

--- lib/ga/index.ts
+++ lib/ga/index.ts
@@ -3,8 +3,10 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageView = (url: URL) => {
   if (GA_TRACKING_ID != null) {
-    window.gtag("config", GA_TRACKING_ID, {
-      send_page_view: url
+    window.gtag('config', GA_TRACKING_ID, {
+      send_page_view: url,
+      cookie_flags: 'SameSite=None;Secure',
+      cookie_domain: 'laadan-dict.vercel.app'
     });
   }
 };
