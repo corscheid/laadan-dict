8aea76a7b77ca80b7e232186329486da5eafa4dc
Corey Scheideman <corscheid@gmail.com>
2021-09-14 20:40:44 -0500

    Add Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app'
+import * as gtag from "../lib/ga";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageView(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
+};
+
+export default App;
